dde11c8aa19a1f56cfd8a4733909e0bb320f1701
richj <richj@oregonstate.edu>
2017-06-05 16:37:49 -0800

    Added the base files, format and created README

---
+++ README.md
@@ -0,0 +1,36 @@
+Tasks:
+
+main pages {
+  main.handlebars { TODO
+    This is the main page template
+    this includes the CSS: TODO
+  }
+
+  search.handlebars { TODO
+    this is our large search page
+  }
+
+  view.handlebars { TODO
+    this is the page where we will do the movie
+  }
+}
+
+templates{
+  comments { TODO
+    we need .handlebars templags for comments
+  }
+
+  movies { TODO
+    we need a templagte for movies
+  }
+}
+
+server {
+  server.js { TODO
+    The big one
+  }
+  
+  index.js { TODO
+    our search function
+  }
+}
---
+++ server.js
@@ -0,0 +1,51 @@
+/*
+* Write your Express server in this file as described in README.md.
+*/
+var path = require('path');
+var fs = require('fs');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var twitsData = require('./twitData');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res, next) {
+
+  var templateArgs = {
+    //render args from home page
+  };
+
+  res.render('twitPage', templateArgs);
+
+});
+
+app.get('/twits/:index', function (req, res, next) {
+  console.log("== url params for request:", req.params.index);
+  var index = req.params.index;
+  var twitData = twitsData[index];// FIXME: this will need to change as index will be the movie name
+  console.log(twitData);
+
+  if (twitData) {
+    var templateArgs = {
+      //render args for single page
+    }
+    res.render('twitPage', templateArgs);
+  }
+  else {
+    next();
+  }
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', function (req, res) {
+  res.status(404).render('404Page');
+});
+
+// Start the server listening on the specified port.
+app.listen(port, function () {
+  console.log("== Server listening on port", port);
+});
